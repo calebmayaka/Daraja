20041e96cb5c30aebadf15d071a155b529986263
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-05-20 09:08:44 +0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,22 +1,13 @@
-"""
-URL configuration for Daraja project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+# adding the include module to facilitate linking to other urls in apps
+from django.urls import path, include
 
+#urlpatterns for defining paths
 urlpatterns = [
+    # this path is for the admin panel. ip/admin. separated by commas
+
     path('admin/', admin.site.urls),
+    # maps to users app urls, empty url ,maps when defualt is opened  
+    path("", include("users.urls") )
 ]
+ 
