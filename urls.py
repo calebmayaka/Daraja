56d8238c1da6f91939171e2e403402a3d4e122ef
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-05-30 09:31:27 +0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 # adding the include module to facilitate linking to other urls in apps
 from django.urls import path, include
 
-#urlpatterns for defining paths
+#urlpatterns for defining paths - it is a list
 urlpatterns = [
     # this path is for the admin panel. ip/admin. separated by commas
 
